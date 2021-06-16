ff5d3b09479684bda00ae2722812e69ec6ca5263
lemon-sazu <lemon.sazu@gmail.com>
2021-06-16 09:10:47 +0600

    Install Vue and add bulma bulma-toast axios with yarn

---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <router-link class="navbar-item" to="#">
+        <strong>Mynul CRM</strong>
+      </router-link>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <router-link class="button is-success" to="#">
+              <strong>Sign up</strong>
+            </router-link>
+            <router-link class="button is-light" to="#"> Log in </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
